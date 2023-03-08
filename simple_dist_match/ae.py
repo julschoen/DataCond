dee99bf4351f83f7fe6eeaeb57da2977c855c9d2
julschoen <julianschoen@gmx.de>
2023-03-08 16:44:21 +0100

    Update ae.py

--- simple_dist_match/ae.py
+++ simple_dist_match/ae.py
@@ -8,66 +8,57 @@ class Encoder(nn.Module):
     def __init__(self, params, z_dim, nfilter=128):
         super(Encoder, self).__init__()
 
-        self.conv1_1 = nn.Conv2d(3, nfilter//2, 4, 2, 1, bias=False)
-        self.norm1 = nn.GroupNorm(nfilter//2, nfilter//2, affine=True)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1)
+        self.norm1 = nn.GroupNorm(16, 16 affine=True)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
+        self.norm2 = nn.GroupNorm(32, 32, affine=True)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
+        self.norm3 = nn.GroupNorm(64, 64, affine=True)
+        self.conv4 = nn.Conv2d(64, z_dim, kernel_size=3, stride=2, padding=1)
 
-        self.conv1_2 = nn.Conv2d(10, nfilter//2, 4, 2, 1, bias=False)
-
-        self.conv2 = nn.Conv2d(nfilter, nfilter*2, 4, 2, 1, bias=False)
-        self.norm2 = nn.GroupNorm(nfilter*2, nfilter*2, affine=True)
-
-        self.conv3 = nn.Conv2d(nfilter*2, nfilter*4, 4, 2, 1, bias=False)
-        self.norm3 = nn.GroupNorm(nfilter*4, nfilter*4, affine=True)
-
-        self.conv4 = nn.Conv2d(nfilter * 4, z_dim, 4, 1, 0, bias=False)
-
-        self.fill = torch.zeros([10, 10, 32, 32]).to(params.device)
-        for i in range(10):
-            self.fill[i, i, :, :] = 1
+        self.fc1 = nn.Linear(10, z_dim)
 
     def forward(self, input, label):
-        label = self.fill[label]
-
-        x = self.norm1(self.conv1_1(input))
-        x = F.leaky_relu(x, 0.2)
-
-        y = F.leaky_relu(self.conv1_2(label), 0.2)
-        x = torch.cat([x, y], 1)
+        x = self.norm1(self.conv1(input))
+        x = F.relu(x)
 
         x = self.norm2(self.conv2(x))
-        x = F.leaky_relu(x, 0.2)
+        x = F.relu(x)
 
         x = self.norm3(self.conv3(x))
-        x = F.leaky_relu(x, 0.2)
+        x = F.relu(x)
+        x = self.conv4(x)
 
-        return self.conv4(x)
+        x = x.view(-1, 128)
+        y = self.fc1(label)
+        x = torch.cat((x, y), dim=1)
+
+        return x
 
 class Decoder(nn.Module):
     # initializers
     def __init__(self, z_dim, nfilter=128):
         super(Decoder, self).__init__()
 
-        self.conv1 = nn.ConvTranspose2d(z_dim, nfilter*4, 4, 1, 0, bias=False)
-        self.norm1 = nn.GroupNorm(nfilter*4, nfilter*4, affine=True)
+        self.conv1 = nn.ConvTranspose2d(z_dim, 64, kernel_size=4, stride=2, padding=1)
+        self.conv2 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)
+        self.conv3 = nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1)
+        self.conv4 = nn.ConvTranspose2d(16, 3, kernel_size=4, stride=2, padding=1)
 
-        self.conv2 = nn.ConvTranspose2d(nfilter*4, nfilter*2, 4, 2, 1, bias=False)
-        self.norm2 = nn.GroupNorm(nfilter*2, nfilter*2, affine=True)
-
-        self.conv3 = nn.ConvTranspose2d(nfilter*2, nfilter, 4, 2, 1, bias=False)
-        self.norm3 = nn.GroupNorm(nfilter, nfilter, affine=True)
-
-        self.conv4 = nn.ConvTranspose2d(nfilter, 3, 4, 2, 1, bias=False)
+        self.norm1 = nn.GroupNorm(64, 64, affine=True)
+        self.norm2 = nn.GroupNorm(32, 32, affine=True)
+        self.norm3 = nn.GroupNorm(16, 16, affine=True)
 
     # forward method
     def forward(self, input):
         x = self.norm1(self.conv1(input))
-        x = F.leaky_relu(x, 0.2)
+        x = F.relu(x)
 
         x = self.norm2(self.conv2(x))
-        x = F.leaky_relu(x, 0.2)
+        x = F.relu(x)
 
         x = self.norm3(self.conv3(x))
-        x = F.leaky_relu(x, 0.2)
+        x = F.relu(x)
         
         return torch.tanh(self.conv4(x))
 
