a58eb06fdfc09fa7a03c221af7bfa62451200006
julschoen <julianschoen@gmx.de>
2023-03-09 13:44:34 +0100

    Update main.py

--- simple_dist_match/main.py
+++ simple_dist_match/main.py
@@ -19,7 +19,7 @@ def main():
 
     ### VAE
     parser.add_argument('--niter_ae', type=int, default=3000)
-    parser.add_argument('--lrAE', type=float, default=1e-4)
+    parser.add_argument('--lrAE', type=float, default=1e-3)
     parser.add_argument('--z_dim', type=int, default=32)
     parser.add_argument('--filter', type=int, default=64)
 
