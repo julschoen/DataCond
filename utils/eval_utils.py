9d1f1009ea8917d0b45298558edb529041f5af04
julschoen <julianschoen@gmx.de>
2023-03-10 13:13:45 +0100

    Update eval_utils.py

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -52,13 +52,13 @@ def test(model, device, test_loader):
 
 class EarlyStopping():
     def __init__(self, tolerance=5, min_delta=1):
-
         self.tolerance = tolerance
         self.min_delta = min_delta
         self.counter = 0
         self.early_stop = False
 
     def __call__(self, train_loss, validation_loss):
+        print(train_loss, validation_loss)
         if (validation_loss - train_loss) > self.min_delta:
             self.counter +=1
             if self.counter >= self.tolerance:  
