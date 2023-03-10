f2eba4e1370987e743a6efb074c9ae4e50eac924
julschoen <julianschoen@gmx.de>
2023-03-10 13:26:56 +0100

    Update eval_utils.py

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -62,7 +62,6 @@ class EarlyStopper():
         self.early_stop = False
 
     def __call__(self, train_loss, validation_loss):
-        print(train_loss, validation_loss)
         if (validation_loss - train_loss) > self.min_delta:
             self.counter +=1
             if self.counter >= self.tolerance:  
