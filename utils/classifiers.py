7e938a3c912e1bfc5c6845f9a5741e44d8e678f8
julschoen <julianschoen@gmx.de>
2023-03-13 15:47:13 +0100

    Update classifiers.py

--- utils/classifiers.py
+++ utils/classifiers.py
@@ -120,28 +120,28 @@ class BasicBlock(nn.Module):
 class ResNet18(nn.Module):
     def __init__(self):
         super(ResNet18, self).__init__()
-        self.in_planes = 64
+        self.in_planes = 32
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
         self.layer1 = nn.Sequential(
-            BasicBlock(64, 64, stride=1),
-            BasicBlock(64, 64, stride=1)
+            BasicBlock(self.in_planes, self.in_planes, stride=1),
+            BasicBlock(self.in_planes, self.in_planes, stride=1)
         )
         self.layer2 = nn.Sequential(
-            BasicBlock(64, 128, stride=2),
-            BasicBlock(128, 128, stride=1)
+            BasicBlock(self.in_planes, self.in_planes*2, stride=2),
+            BasicBlock(self.in_planes*2, self.in_planes*2, stride=1)
         )
         self.layer3 = nn.Sequential(
-            BasicBlock(128, 256, stride=2),
-            BasicBlock(256, 256, stride=1)
+            BasicBlock(self.in_planes*2, self.in_planes*4, stride=2),
+            BasicBlock(self.in_planes*4, self.in_planes*4, stride=1)
         )
         self.layer4 = nn.Sequential(
-            BasicBlock(256, 512, stride=2),
-            BasicBlock(512, 512, stride=1)
+            BasicBlock(self.in_planes*4, self.in_planes*8, stride=2),
+            BasicBlock(self.in_planes*8, self.in_planes*8, stride=1)
         )
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.linear = nn.Linear(512, 10)
+        self.linear = nn.Linear(self.in_planes*8, 10)
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
