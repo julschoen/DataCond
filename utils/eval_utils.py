2f43ae889851d9d19c23c8a6c4162514495ead75
julschoen <julianschoen@gmx.de>
2023-03-13 15:51:33 +0100

    Update eval_utils.py

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -77,9 +77,9 @@ class EarlyStopper():
             self.counter += 1
             if self.counter >= self.patience:
                 self.early_stop = True
-        elif score < self.best_score:
+        else:#if score < self.best_score:
             self.best_score = score
             self.best_weights = model.state_dict()
             self.counter = 0
-        else:
-            self.counter=0
+        #else:
+        #    self.counter=0
