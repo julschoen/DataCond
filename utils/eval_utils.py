7814509603bf7f2aec6edcf7c98ee82deba11365
julschoen <julianschoen@gmx.de>
2023-03-13 15:33:23 +0100

    Update eval_utils.py

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -69,11 +69,11 @@ class EarlyStopper():
 
     def __call__(self, val_loss, model):
         print(val_loss)
-        score = -val_loss
+        score = val_loss
         if self.best_score is None:
             self.best_score = score
             self.best_weights = model.state_dict()
-        elif score < self.best_score + self.delta:
+        elif score > self.best_score + self.delta:
             self.counter += 1
             if self.counter >= self.patience:
                 self.early_stop = True
